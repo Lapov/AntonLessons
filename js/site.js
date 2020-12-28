7e313c5096521968efe2a4c6fb14a443fb59a158
lapov <LapovAndrey@gmail.com>
2020-12-28 18:55:36 +0000

    first commit

---
+++ js/site.js
@@ -0,0 +1,23 @@
+﻿// Write your Javascript code.
+
+function successSend(data, textStatus, jqXHR) {
+    if (textStatus === undefined)
+        return;
+
+    if (data === "OK") {
+        $("#ErrorMessage").hide();
+        $("#SendForm").hide(1000, arguments.callee);
+        $("#SuccessMessage").show(1000);
+    }
+    else {
+        $("#ErrorMessage").text(data);
+        $("#ErrorMessage").show();
+    }
+};
+
+function failSend(data, textStatus, jqXHR) {
+    $("#SendForm").hide(1000, arguments.callee);
+    $("#ErrorMessage").show(1000);
+};
+
+//$(document).ready(function () { t268_init('14916384'); }); $('.t268').bind('displayChanged', function () { t268_init('14916384'); });
